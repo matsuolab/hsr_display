70800b279473e8b10c4d60b38b3e24d196549bc2
hellososhi <22724971+hellososhi@users.noreply.github.com>
2023-10-22 19:02:02 +0900

    サーバーが起動するまで待ってからブラウザを起動する

--- RUN_DOCKER_CONTAINER.sh
+++ RUN_DOCKER_CONTAINER.sh
@@ -1,2 +1,8 @@
 docker-compose -p hsr_display -f ./docker-compose.yml up -d
+
+# wait for the server to start
+while ! nc -z localhost 3000; do
+  sleep 0.1
+done
+
 firefox --kiosk http://localhost:3000
